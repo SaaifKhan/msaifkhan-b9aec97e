311ca7f7597ae0b0052a85c0293dbf40a09635ab
M.Saif khan <25413061+SaaifKhan@users.noreply.github.com>
2026-03-31 13:44:08 +0500

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "/msaifkhan-b9aec97e/",
   plugins: [react()],
   test: {
     environment: "jsdom",
